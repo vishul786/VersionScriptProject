2bff33dc447eceffc51b53ec9f30a107c0d802b2
vishul786 <vishulk786@gmail.com>
2025-04-22 23:33:08 +0530

    Bumped up version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,2 @@
-plugins {
-    id("com.android.application")
-    kotlin("android")
-}
-
-android {
-    namespace = "com.example.versionscriptproject"
-    compileSdk = 33
-
-    defaultConfig {
-        applicationId = "com.example.versionscriptproject"
-        minSdk = 21
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0.0"
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-}
+rootProject.name = "VersionScriptProject"
+include(":app")
